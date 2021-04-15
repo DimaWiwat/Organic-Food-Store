cb090c3553827d62f3174f3b68ae1f218dc22e24
Dmitry <d.wygoniajlo@gmail.com>
2021-04-15 18:57:49 +0300

    added slider

---
+++ app.js
@@ -0,0 +1,10 @@
+$(document).ready(function(){
+$('.slider').slick({
+   arrows:false,
+   dots:true,
+   adaptiveHeight:true,
+   slideToShow:1,
+   autoplay: true,
+   autoplaySpeed:1500,
+});
+});
